e6d61b09896e08214a0f4795ced75b3729b43b3e
Marc Rene Arns <arns@stridor.net>
2015-02-07 01:37:12 +0100

    update wsi

--- .vendor/src/gopkg.in/go-on/wsi.v1/json.go
+++ .vendor/src/gopkg.in/go-on/wsi.v1/json.go
@@ -1,8 +1,12 @@
 package wsi
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
+	"reflect"
+
+	"gopkg.in/go-on/lib.v2/internal/meta"
 )
 
 // JSONStreamer streams a json array to an http.ResponseWriter
@@ -10,6 +14,46 @@ type JSONStreamer struct {
 	w     http.ResponseWriter
 	enc   *json.Encoder
 	first bool
+}
+
+// MapViaJSON transforms an object to its map representation via
+// json marshalling.
+// MapViaJSON is not fast (doing a json encoding and decoding each time),
+// but it is convenient and universal somehow
+func MapViaJSON(v interface{}) (m map[string]interface{}, err error) {
+	var bf bytes.Buffer
+	err = json.NewEncoder(&bf).Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	m = map[string]interface{}{}
+	err = json.NewDecoder(&bf).Decode(&m)
+	return
+}
+
+func MustMapViaJSON(v interface{}) map[string]interface{} {
+	m, err := MapViaJSON(v)
+	if err != nil {
+		panic(err.Error())
+	}
+	return m
+}
+
+// MapSQL similar to MapViaJSON (but should be faster), but for sql tags. structPtr must be a pointer to a struct
+func MapSQL(structPtr interface{}) (map[string]interface{}, error) {
+	s, err := meta.StructByValue(reflect.ValueOf(structPtr))
+	if err != nil {
+		return nil, err
+	}
+	return s.ToMap("sql"), nil
+}
+
+func MustMapSQL(structPtr interface{}) map[string]interface{} {
+	m, err := MapSQL(structPtr)
+	if err != nil {
+		panic(err.Error())
+	}
+	return m
 }
 
 // NewJSONStreamer returns a JSONStreamer for the given ResponseWriter and starts writing to it.
